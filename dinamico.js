f628b882ee042cdbfb712ab14ed0bcdc5a648e72
DiegoZamora33 <61322779+DiegoZamora33@users.noreply.github.com>
2020-04-10 02:41:12 -0500

    Maquetado Dinamico. Trabajar sbre los modulos independientes

--- dinamico.js
+++ dinamico.js
@@ -6,10 +6,13 @@ $(document).ready(function ()
     });
 
 
-    $("#carga").click(function() 
+    $("#home").click(function() 
     {
+
+      miOff();
+      this.className = "active";
         $.ajax({
-          url: "chartkkk.html" ,
+          url: "home.html" ,
 
           success: function(data){
             setTimeout(function(){
@@ -22,4 +25,98 @@ $(document).ready(function ()
 
       });
 
+    $("#competidores").click(function() 
+    {
+      miOff();
+      this.className = "active";
+        $.ajax({
+          url: "competidores.html" ,
+
+          success: function(data){
+            setTimeout(function(){
+              $("#mostrador").html(data);
+            }
+          );
+        }
+
+        });
+
+      });
+
+    $("#entrenadores").click(function() 
+    {
+
+      miOff();
+      this.className = "active";
+        $.ajax({
+          url: "entrenadores.html" ,
+
+          success: function(data){
+            setTimeout(function(){
+              $("#mostrador").html(data);
+            }
+          );
+        }
+
+        });
+
+      });
+
+    $("#reportes").click(function() 
+    {
+
+      miOff();
+      this.className = "active";
+        $.ajax({
+          url: "reportes.html" ,
+
+          success: function(data){
+            setTimeout(function(){
+              $("#mostrador").html(data);
+            }
+          );
+        }
+
+        });
+
+      });
+
+    $("#estadisticas").click(function() 
+    {
+
+      miOff();
+      this.className = "active";
+        $.ajax({
+          url: "estadisticas.html" ,
+
+          success: function(data){
+            setTimeout(function(){
+              $("#mostrador").html(data);
+            }
+          );
+        }
+
+        });
+
+      });
+
+    $("#competencias").click(function() 
+    {
+      
+      miOff();
+      this.className = "active";
+    });
+
 });
+
+function miOff()
+{
+  document.getElementById('home').className = " ";
+  document.getElementById('competidores').className = " ";
+  document.getElementById('entrenadores').className = " ";
+  document.getElementById('reportes').className = " ";
+  document.getElementById('estadisticas').className = " ";
+  document.getElementById('competencias').className = " ";
+}
+
+
